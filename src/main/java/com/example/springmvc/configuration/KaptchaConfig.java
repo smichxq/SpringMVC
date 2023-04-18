c7544697eb39f28bee0f61dd50f40a97c972e1d8
tjustb184130225 <smith.xue@foxmail.com>
2023-04-18 12:35:53 +0800

    KaptchaConfig

---
+++ src/main/java/com/example/springmvc/configuration/KaptchaConfig.java
@@ -0,0 +1,32 @@
+package com.example.springmvc.configuration;
+
+import com.google.code.kaptcha.Producer;
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+@Configuration
+public class KaptchaConfig {
+
+    @Bean
+    public Producer getProducer() {
+        DefaultKaptcha producer = new DefaultKaptcha();
+        Properties properties = new Properties();
+        properties.setProperty("kaptcha.image.width","100");
+        properties.setProperty("kaptcha.image.height", "40");
+        properties.setProperty("kaptcha.textproducer.font.size", "32");
+        properties.setProperty("kaptcha.textproducer.font.color", "0,0,0");
+        properties.setProperty("kaptcha.textproducer.char.string", "0123456789qwertyuiopasdfghjklzxcvbnm");
+        properties.setProperty("kaptcha.textproducer.char.length", "4");
+        properties.setProperty("kaptcha.noise.impl", "com.google.code.kaptcha.impl.NoNoise");
+
+
+        producer.setConfig(new Config(properties));
+
+        return producer;
+
+    }
+}
---
+++ src/main/java/com/example/springmvc/controller/CookieDemo.java
@@ -0,0 +1,49 @@
+package com.example.springmvc.controller;
+
+import com.example.springmvc.util.CommonUtil;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/cookie")
+public class CookieDemo {
+
+    @RequestMapping(value = "/set",method = RequestMethod.GET)
+    @ResponseBody
+    public String setCookie(HttpServletResponse response) {
+        Cookie cookie = new Cookie("currentcok", CommonUtil.UUID().substring(0,8));
+        cookie.setPath("/community/cookie");
+        //设置失效时间，使用持久化存放
+//        cookie.setMaxAge(60 * 10);
+        response.addCookie(cookie);
+        return "set cookie";
+    }
+
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
+    @ResponseBody
+    public String getCookie(@CookieValue("currentcok") String cok) {
+
+        return cok;
+    }
+
+    @RequestMapping(value = "/session/set",method = RequestMethod.GET)
+    @ResponseBody
+    public String setSession(HttpSession session) {
+        session.setAttribute("name","session1");
+        session.setAttribute("value",1);
+        return "set session";
+    }
+
+    @RequestMapping(value = "/session/get",method = RequestMethod.GET)
+    @ResponseBody
+    public String getSession(HttpSession session) {
+
+        return "sessionId:" + session.getId() + " " + "name:" + session.getAttribute("name") + "" + "value:" + session.getAttribute("value");
+    }
+}
